06f84621c07593a9eef0dd0f332c554570d6a18a
ROY <lwrk28@mira1.dur.ac.uk>
2023-11-15 23:02:44 +0000

    Getting faster

--- magic_matrix_gpu.cu
+++ magic_matrix_gpu.cu
@@ -166,7 +166,7 @@ bool isMagicSquare(int** matrix, int N)
     } */
 
     // compute sum of elements on main diagonal
-    #pragma omp parallel for num_threads(4) reduction(+:main_diag_sum)
+    #pragma omp parallel for num_threads(4) nowait reduction(+:main_diag_sum)
     for (int i = 0; i < N; i++)
     {
         main_diag_sum += matrix[i][i];
@@ -178,7 +178,7 @@ bool isMagicSquare(int** matrix, int N)
     } */
 
     // compute sum of elements on antidiagonal
-    #pragma omp parallel for num_threads(4) reduction(+:anti_diag_sum)
+    #pragma omp parallel for num_threads(4) nowait reduction(+:anti_diag_sum)
     for (int i = 0; i < N; i++)
     {
         anti_diag_sum += matrix[i][N - 1 - i];
