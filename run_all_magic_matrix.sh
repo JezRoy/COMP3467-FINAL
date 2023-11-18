c497722b2346f9a7275a3eb5e6b33907de063127
Jeremy Roy <jezoffy@gmail.com>
2023-11-18 16:17:53 +0000

    debug

--- run_all_magic_matrix.sh
+++ run_all_magic_matrix.sh
@@ -23,8 +23,12 @@ EXECUTABLE="./mm"
 
 # Execute for N = 3
 echo "Running magic_matrix for N=3..."
-srun --exclusive -n1 $EXECUTABLE $dir/pattern3x3.dat $dir/modifier3x3.dat > magic_matrix_output_3x3.txt &
+srun --exclusive -n1 $EXECUTABLE $dir/pattern3x3.dat $dir/modifier3x3.dat > magic_matrix_output_3x3.out &
 
 # Execute for N = 10
 echo "Running magic_matrix for N=10..."
-srun --exclusive -n1 $EXECUTABLE $dir/pattern10x10.dat $dir/modifier10x10.dat > magic_matrix_output_10x10.txt &
+srun --exclusive -n1 $EXECUTABLE $dir/pattern10x10.dat $dir/modifier10x10.dat > magic_matrix_output_10x10.out &
+
+wait
+
+make clean
