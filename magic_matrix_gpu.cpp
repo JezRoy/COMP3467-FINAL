14de307d3285d5e70a9106c677a2b5a9b024f070
Jeremy Roy <jezoffy@gmail.com>
2023-11-18 16:38:08 +0000

    debug

--- magic_matrix_gpu.cpp
+++ magic_matrix_gpu.cpp
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <unordered.set.h>
+#include <unordered_set.h>
 #include <omp.h>
 
 // The generateMagicSquare() function is supposed to generate a large matrix square from two smaller ones.
