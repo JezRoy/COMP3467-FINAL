2d3c19240f2adcd521229ca344447cb768133ad6
Jeremy Roy <jezoffy@gmail.com>
2023-11-19 18:25:15 +0000

    debug

--- install_numact1.sh
+++ install_numact1.sh
@@ -1,1 +1,24 @@
-https://github.com/numactl/numactl/archive/master.zip
+# FROM: https://github.com/numactl/numactl/archive/master.zip
+
+
+#!/bin/bash
+
+# Download the numactl source code from GitHub
+wget https://github.com/numactl/numactl/archive/master.zip
+
+# Unzip the downloaded file
+unzip master.zip
+
+# Enter the numactl directory
+cd numactl-master
+
+# Configure, build, and install numactl locally in the home directory
+./autogen.sh
+./configure --prefix=$HOME/numactl_install
+make
+make install
+
+# Display information about the system's memory architecture using numactl -H
+$HOME/numactl_install/bin/numactl -H > numactl.out
+
+echo "numactl installed and information saved in numactl.out"
