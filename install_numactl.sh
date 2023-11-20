cd7afd80c8d70a4f90f12625f92aafe3a5c03430
Jeremy Roy <jezoffy@gmail.com>
2023-11-20 19:29:01 +0000

    instal numactl

--- install_numactl.sh
+++ install_numactl.sh
@@ -1,25 +1,32 @@
 # FROM: https://github.com/numactl/numactl/archive/master.zip
 
-
+directory = "/home/"
 #!/bin/bash
 
 # Download the numactl source code from GitHub
-wget https://github.com/numactl/numactl/archive/master.zip
+wget https://github.com/numactl/numactl/archive/master.zip -d "$directory"
 sudo apt-get install libnuma-dev
 
 # Unzip the downloaded file
-unzip master.zip
+unzip "$directory/master.zip"
 
 # Enter the numactl directory
-cd numactl-master
+cd "$directory/numactl-master"
 
 # Configure, build, and install numactl locally in the home directory
 ./autogen.sh
-./configure --prefix=$HOME/numactl_install
-make
-make install
+./configure
+make install DESTDIR="$directory"
+
+echo "numactl installed at $directory/numactl-master"
+
+export PATH="$PATH:$directory/numactl-master/"
+
+echo "numactl has now been added to the path"
 
 # Display information about the system's memory architecture using numactl -H
 $HOME/numactl_install/bin/numactl -H > numactl.out
 
 echo "numactl installed and information saved in numactl.out"
+
+cd home2
